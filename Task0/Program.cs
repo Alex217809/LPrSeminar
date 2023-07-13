5ed95ac00e06f4fbcf28eea4b3febec2447859f5
Alex777 <alex21210978@gmail.com>
2023-07-13 03:18:38 +0300

    Задача методы

---
+++ Task0/Program.cs
@@ -0,0 +1,41 @@
+﻿// На входе число на выходе квадрат
+// int number = 5;
+// int result =  number*number;
+// Console.WriteLine(result);
+
+
+//Два числа на входе на выходе квадрат 
+// Числа вводятся с консоли
+// Console.WriteLine ("введите первое число");
+// int number_f = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine ("введите второе число");
+// int number_s = Convert.ToInt32(Console.ReadLine());
+
+// if (number_f == number_s*number_s)
+// {
+//     Console.WriteLine ("Первая переменная является квадратом второй");
+// }
+// else
+// {
+//     Console.WriteLine ("Первая переменная Не является квадратом второй");
+// }
+
+
+//если number_f == number_s*number_s
+// занчит равно
+
+
+// Напишите программу вычисляющую значение функции числа в квадрате
+
+int Power(int number)
+{
+    int result = number*number;
+    return result;
+}
+// функция
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Power(num));
