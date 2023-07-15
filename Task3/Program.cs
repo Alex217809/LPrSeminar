1605fa9e67d024cc4067450830fdf95dac3b39fe
Alex777 <alex21210978@gmail.com>
2023-07-15 23:43:56 +0300

    Задача с тремя цифрами

---
+++ Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿void Task9()
+{
+    // напишите программу которая выводи случайное число из
+    // отрезка 10 1000 и и сделайте из него двухзначное убрав посередине цифру
+   
+    int number = new Random().Next(10, 1000); 
+
+    Console.WriteLine($"выпало случайное число {number}");
+
+    int f_digit = number / 100;
+    int s_digit = number % 100;
+    int t_digit = number % 10;
+
+    Console.WriteLine($"первая цифра {f_digit}");
+
+    Console.WriteLine($"вторая цифра {s_digit}");
+
+     Console.WriteLine($"третья цифра {t_digit}");
+
+
+    Console.WriteLine($"двухзначное число {f_digit}{t_digit}");
+}
+
+Task9();
