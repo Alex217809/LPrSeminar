8cf986b23fbbc4ec44a820c14471b09c88943d5b
Alex777 <alex21210978@gmail.com>
2023-07-19 23:22:11 +0300

    новое семинарское занятие

---
+++ Task4/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Task17()
+{
+    //Программа задает координаты x и y 
+    //определить четверть
+    int input(string text)
+    {
+        Console.WriteLine(text);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    int x=input("Введите X: ");
+    int y=input("Введите Y: ");
+     if (x > 0 && y > 0)
+     {
+        Console.WriteLine("Точка принадлежит первой четверти");
+     }
+     else if (x < 0 && y > 0)
+     
+     {
+        Console.WriteLine("Точка принадлежит второй четверти");
+     }
+     else if (x < 0 && y < 0)
+     {
+        Console.WriteLine("Точка принадлежит третьей четверти");
+     }
+     else if (x > 0 && y < 0)
+     {
+        Console.WriteLine("Точка принадлежит четвертой четверти");
+     }
+     else 
+     {
+        Console.WriteLine("Точка лежит на осях");
+     }
+}
+
+Task17();
+
